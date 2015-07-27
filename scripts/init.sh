c476e0540b6d34f0f3abf0d1b9b4c3af81d0334d
beaudryj <beaudryj@wit.edu>
2015-07-27 15:06:25 -0400

    update sshd

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,8 @@ set -eou pipefail
 
 useradd -m $USER -p $PASS -s /bin/bash 
 echo '$USER ALL=(ALL:ALL)ALL' > /etc/sudoers.d/01_$USER
-echo '$USER:$PASS' | chpasswd 
+echo 'root:$PASS' | chpasswd 
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 supervisord -n
