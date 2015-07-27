b92879b5c46132b75da1d403ee56faccc5bcdb9a
beaudryj <beaudryj@wit.edu>
2015-07-27 12:59:15 -0400

    updated sshd

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 set -eou pipefail
 
-useradd -m $USER
-echo "$USER:$PASS" | chpasswd
+useradd -m $USER -p $PASS
 echo "$USER ALL=(ALL:ALL)ALL" > /etc/sudoers.d/01_$USER
+
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
 supervisord -n
