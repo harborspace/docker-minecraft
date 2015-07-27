8947f876e53ebd39e219e433d1f20ad3636e1d06
beaudryj <beaudryj@wit.edu>
2015-07-27 15:58:31 -0400

    finalized sshd

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 privs="ALL=(ALL:ALL) ALL"
 
-useradd -m $USER -p $PASS -s /bin/bash 
+useradd -m $USER -p $PASS -s /bin/bash -d /data
 echo $USER" "$privs > /etc/sudoers.d/01_$USER
 echo $USER":"$PASS | chpasswd 
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
