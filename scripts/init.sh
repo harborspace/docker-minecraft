01448e2066e6257f414818fe6edd6e4b11af82d1
beaudryj <beaudryj@wit.edu>
2015-07-27 13:44:37 -0400

    test sshd

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -eou pipefail
 
-useradd -m $USER -p $PASS
+useradd -m $USER -p shipyard655
 echo "$USER ALL=(ALL:ALL)ALL" > /etc/sudoers.d/01_$USER
 
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
