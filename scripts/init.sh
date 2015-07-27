0ed34a8478c955ab6fa39222094c39021aa2fbe7
beaudryj <beaudryj@wit.edu>
2015-07-27 15:34:21 -0400

    sshd vars

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 set -eou pipefail
 
+privs="ALL=(ALL:ALL) ALL"
+
 useradd -m $USER -p $PASS -s /bin/bash 
-echo '$USER ALL=(ALL:ALL)ALL' > /etc/sudoers.d/01_$USER
-echo 'root:test123' | chpasswd 
+echo $USER" "$privs > /etc/sudoers.d/01_$USER
+echo $USER":"$PASS | chpasswd 
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 supervisord -n
